44a157fae730498c6fccc7a26aa00467c8f7479e
Helfer Thomas <thomas.helfer@cea.fr>
2018-03-10 08:15:24 +0100

    More work on importing behaviour in MTest major mode. Start working on the Implicit DSL wizard

---
+++ packages/mtest/include/QEmacs/MaterialPropertyDescription.hxx
@@ -0,0 +1,39 @@
+/*!
+ * \file   MaterialPropertyDescription.hxx
+ * \brief    
+ * \author th202608
+ * \date   05/03/2018
+ */
+
+#ifndef LIB_MATERIALPROPERTYDESCRIPTION_HXX
+#define LIB_MATERIALPROPERTYDESCRIPTION_HXX
+
+#include <QtCore/QString>
+
+namespace qemacs{
+
+  /*!
+   * \brief structure describing a constant material property
+   */
+  struct ConstantMaterialPropertyDescription {
+    QString name;
+    double value;
+  };  // end of struct ConstantMaterialPropertyDescription
+
+  /*!
+   * \brief structure describing a `Cast3M` material property
+   */
+  struct CastemMaterialPropertyDescription {
+    QString name;
+    QString library;
+    QString function;
+  };  // end of struct CastemMaterialPropertyDescription
+
+  //! a simple alias
+  using MaterialPropertyDescription =
+      tfel::utilities::GenType<ConstantMaterialPropertyDescription,
+                               CastemMaterialPropertyDescription>;
+
+} // end of namespace qemacs
+
+#endif /* LIB_MATERIALPROPERTYDESCRIPTION_HXX */
