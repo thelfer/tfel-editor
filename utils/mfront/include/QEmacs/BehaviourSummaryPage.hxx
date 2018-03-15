cef0c9e76441523377af6770e92d8c30335b2e88
Helfer Thomas <thomas.helfer@cea.fr>
2018-03-15 18:39:24 +0100

    more work on MFront behaviour import

---
+++ utils/mfront/include/QEmacs/BehaviourSummaryPage.hxx
@@ -0,0 +1,34 @@
+/*!
+ * \file   BehaviourSummaryPage.hxx
+ * \brief    
+ * \author th202608
+ * \date   15/03/2018
+ */
+
+#ifndef LIB_BEHAVIOURSUMMARYPAGE_HXX
+#define LIB_BEHAVIOURSUMMARYPAGE_HXX
+
+#include <QtWidgets/QWizardPage>
+
+namespace qemacs {
+
+  //! Page summarizing the selected behaviour
+  struct BehaviourSummaryPage : public QWizardPage {
+    /*!
+     * \brief constructor
+     * \param[in] p: parent widget
+     */
+    BehaviourSummaryPage(QWidget* const = nullptr);
+
+    int nextId() const override;
+
+    // destructor
+    ~BehaviourSummaryPage() override;
+
+   private:
+    Q_OBJECT
+  };  // end of BehaviourSummaryPage
+
+} // end of namespace qemacs
+
+#endif /* LIB_BEHAVIOURSUMMARYPAGE_HXX */
---
+++ utils/mfront/src/BehaviourSummaryPage.cxx
@@ -0,0 +1,22 @@
+/*!
+ * \file   BehaviourSummaryPage.cxx
+ * \brief    
+ * \author Thomas Helfer
+ * \date   15/03/2018
+ */
+
+#include "QEmacs/BehaviourSummaryPage.hxx"
+
+namespace qemacs {
+
+  BehaviourSummaryPage::BehaviourSummaryPage(QWidget *const p)
+      : QWizardPage(p) {
+  }  // end of BehaviourSummaryPage::BehaviourSummaryPage
+
+  int BehaviourSummaryPage::nextId() const {
+    return -1;
+  } // end of BehaviourSummaryPage::nextId()
+
+  BehaviourSummaryPage::~BehaviourSummaryPage() = default;
+
+}  // end of namespace qemacs
